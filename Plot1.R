f4db0f8a45ec05a2ee3a15c9424e7bb31101bb19
aperelson@gmail.com <aperelson@gmail.com>
2016-11-19 16:41:40 +0800

    Plot 1 done.

---
+++ Plot1.R
@@ -0,0 +1,43 @@
+processData <- function() 
+{
+  ###########################################################################
+  ## 1 Plot 1 for the assignment at the end of week 1 of the EDA course.   ##
+  ## 2 Retrieves data from the  UC Irvine Machine Learning Repository.     ##
+  ###########################################################################
+  
+  if (!file.exists("data")) 
+  { 
+    stop("Data folder not found!")
+  }
+  
+  ## Use the data.table library to read a large data table:
+  library(data.table)
+  library(dplyr)
+  
+  ## Fetch the household power consumption data:
+  ## Only fetching the 2 days we need:
+  householdData <- fread("data/household_power_consumption.txt",skip = 66637, nrows = 2880)
+  
+  names(householdData) <- c("Date",
+                            "Time",
+                            "Global_active_power",
+                            "Global_reactive_power",
+                            "Voltage",
+                            "Global_intensity",
+                            "Sub_metering_1",
+                            "Sub_metering_2",
+                            "Sub_metering_3")
+  
+  ## convert date and time column to a date/time for plotting:
+  householdData <- mutate(householdData, newDatetime = as.POSIXct(strptime(paste(householdData$Date, householdData$Time), format = "%d/%m/%Y %H:%M:%S")))
+
+  png(filename="Plot1.png", width = 480, height = 480)
+
+  with(householdData, 
+       hist(Global_active_power, 
+            col="red", 
+            xlab="Global Active Power (Kilowatts)", 
+            main="Global Active Power"))
+  
+  dev.off()
+}
